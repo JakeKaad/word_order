e300a61eb5f841399919d34ea6ae50b742d37f3b
Jake Kaad and Jeyananthan Sivakumaran <nayagan@gmail.com>
2015-02-12 11:47:36 -0800

    Add full functionality for wordOrder

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+var wordOrder = function(phrase) {
+  var words = phrase.split(" ");
+  var sortedWords = phrase.split(" ").sort();
+  var wordCounter = {};
+  var output = [];
+  words.forEach(function(word){
+    wordCounter[word] = 1;
+  });
+
+  for(var i=0; sortedWords.length > i; i++){
+    if (sortedWords[i] === sortedWords[i+1]){
+      wordCounter[sortedWords[i]] += 1;
+      delete sortedWords[i];
+    }
+  }
+
+  for(var i = 15; i > 0; i--){
+    for(var key in wordCounter){
+      if (wordCounter[key] === i) {
+        if( isATie(wordCounter[key], wordCounter[output[output.length - 1 ]])){
+          // debugger;
+          if(words.indexOf(key)<words.indexOf(wordCounter[output[output.length - 1 ]]) && sortedWords.indexOf(key)>words.indexOf(wordCounter[output[output.length - 1 ]])) {
+            var shifted = output.pop();
+            output.push(key.toString());
+            output.push(shifted);
+          }else{
+            output.push(key.toString());
+
+          }
+        } else {
+          output.push(key.toString());
+        }
+      }
+    }
+  }
+
+// debugger;
+  return output.join(", ");
+};
+
+var isATie = function(freq1, freq2){
+  return freq1===freq2;
+};
---
+++ spec/specs.js
@@ -0,0 +1,21 @@
+describe('wordOrder', function(){
+  it("returns one word if its the only word entered", function(){
+    expect(wordOrder("world")).to.equal("world");
+  });
+
+  it("returns the words in order of appearance if there are no duplicates,", function(){
+    expect(wordOrder("hello world")).to.equal("hello, world");
+  });
+
+  it("returns the words ordered by frequency", function(){
+    expect(wordOrder("hello world world")).to.equal("world, hello");
+  });
+
+  it("returns the words ordered by frequency and if equal frequency the tie-breaker is first appearance", function(){
+    expect(wordOrder("world hello")).to.equal("world, hello");
+  });
+
+  it("returns the words ordered by frequency and if equal frequency the tie-breaker is first appearance", function(){
+    expect(wordOrder("world hello world hello world hello xyz apple")).to.equal("world, hello, xyz, apple");
+  });
+});
